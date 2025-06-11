e0f9842aad85c560798834a4141725c8655559ec
Imam Sayyid Rahman <imamsayyid332@gmail.com>
2025-06-12 00:23:34 +0700

    Configure multi-page build for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        // This tells Vite to build two separate pages
+        main: resolve(__dirname, 'index.html'),
+        viewer: resolve(__dirname, 'viewer.html'),
+      },
+    },
+  },
 })
